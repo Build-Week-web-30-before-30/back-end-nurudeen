efa1ec00dfcd07875f7cc6d5e22b1f9205bd33b7
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-18 20:00:37 +0100

    post and get todos by board id completed

--- routes/board-router.js
+++ routes/board-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const verifyToken = require('../middlewares/verifyToken')
 const boardsHelpers = require('../models/boards-model')
+const todoHelper = require('../models/todos-model')
 
 const router = express.Router();
 
@@ -53,6 +54,35 @@ router.get('/:id/feedback', verifyToken, async (req, res) => {
     );
   } catch(err) {
     res.status(500).json({ message: `failed to fetch feedback for board ${req.params.id}`})
+  }
+})
+
+router.get('/:id/todo', async (req, res) => {
+  try {
+    const todos= await todoHelper.getByBoard(req.params.id);
+
+    res.status(200).json(todos);
+  } catch(err) {
+    res.status(500).json({ message: 'failed to get todos for this board'})
+  }
+})
+
+router.post('/:id/todo', verifyToken, async (req, res) => {
+  try {
+    const newTodo = ({
+      name: req.body.name,
+      completed: req.body.completed,
+      deadline: req.body.deadline,
+      board_id: req.params.id
+    })
+    const todo = await todoHelper.add(newTodo);
+
+    res.status(201).json({
+      message: 'todo added successfully',
+      todo: todo
+    });
+  } catch(err) {
+    res.status(500).json({ message: 'failed to insert new todo'})
   }
 })
 
